58fb4e0133688c01343e4aeb8b03f1faac4e0c16
sun-asterisk <fritzgeralddumdum@github.com>
2021-11-17 20:09:09 +0800

    CRUD integration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,52 @@
 class ArticlesController < ApplicationController
+  # assign set_article to specific method
+  before_action :set_article, only: [:edit, :update, :destroy]
+
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      # flash short hand assignment
+      redirect_to articles_path, notice: 'New article has been created.'
+    else
+      # flash long hand assignment
+      flash[:error] = 'New article has been created.'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+  
+  def article_params
+    params.required(:article).permit(:title, :author, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   validates :title, :author, :content, presence: true
+
+  def full_name
+    "#{title} - #{author}"
+  end
 end
